c80f6a96a13caf6699ec3632e0aa9c29bb4a5273
Leonel Fernandez <leoofernandez@gmail.com>
2024-05-11 16:08:08 -0300

    primer commit

---
+++ js/js.js
@@ -0,0 +1,9 @@
+document.getElementById("cambiarTema").addEventListener("click", cambiarModo);
+
+function cambiarModo () {
+
+    document.querySelector(".body").classList.toggle("modoOscuro");
+
+    
+
+}
